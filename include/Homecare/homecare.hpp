a1afc4790b3f5bfc7a086f0f2c7e76a1284c386b
hoergems <hoergems@gmail.com>
2016-08-30 16:47:16 +1000

    Added homecare folder

---
+++ include/Homecare/homecare.hpp
@@ -0,0 +1,15 @@
+#ifndef __HOMECARE_HPP__
+#define __HOMECARE_HPP__
+#include <robot_headers/robot.hpp>
+namespace shared
+{
+class Homecare: public Homecare
+{
+public:
+    Homecare();
+
+};
+}
+
+
+#endif
